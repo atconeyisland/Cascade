9c48f1ecfd0e98231d453b074192ab0f1e70c2f7
atconeyisland <spectrumat246@gmail.com>
2026-04-08 17:44:10 +0530

    fixed app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from src.cascade_env.server import app
 import uvicorn
 
-if __name__ == "__main__":
+def main():
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
